b3ef248112a52ea7b8950589ca876f3416aa673e
Juliana Nonaka Udvari <111877563+juliana-nonaka@users.noreply.github.com>
2022-11-03 15:15:13 -0300

    Criando Navbar do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Footer/>
+  <Navbar />
+
     </>
   );
 }
