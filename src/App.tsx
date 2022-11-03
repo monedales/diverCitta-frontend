27eaa4649dc62b1070b8ef13f5eb96bf6bb06d94
letícia moneda <98550351+monedales@users.noreply.github.com>
2022-11-03 19:06:43 -0300

    Add files via upload
    Criação da página Home - contendo arquivo home.tsx e home.css

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       <Routes>
 
-        <Route path="/" element={<Login  />} />
+        <Route path="/" element={<Login />} />
 
         <Route path="/home" element={<Home />} />
 
