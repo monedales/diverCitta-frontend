9ef64ca31c65b18408294fe134e88a948c3a7cca
Paula Mayumi <62121416+mayumisiano@users.noreply.github.com>
2022-11-03 15:33:28 -0300

    Desenvolvendo a página Sobre Nós

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
+//import Navbar from './components/estaticos/navbar/Navbar';
+//import Footer from './components/estaticos/footer/Footer';
+//import Home from './paginas/home/Home';
+import Sobre from './paginas/Sobre/Sobre'
 import './App.css';
 
-function App() {
-  return (
-    <>
-  <Navbar />
 
-    </>
+function App() {
+  return (  
+      <Sobre />
   );
 }
 
