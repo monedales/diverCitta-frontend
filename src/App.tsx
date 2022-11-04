aed7041b9c3cfbca9a03cb9ca3f3987455ffb5f3
Paula Mayumi <62121416+mayumisiano@users.noreply.github.com>
2022-11-03 22:36:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import Login from './paginas/login/Login';
-import Sobre from './paginas/Sobre/Sobre'
-
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-
-        <Route path="/" element={<Login  />} />
-
-        <Route path="/home" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/sobre-nos" element={<Sobre />}
-
-      </Routes>
-      <Footer />
-    </Router>
+    
+  <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
 
   );
 }
