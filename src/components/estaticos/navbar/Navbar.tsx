fc8afacd90d49d73ad4e1076b8ca504580b1d236
Paula Mayumi <62121416+mayumisiano@users.noreply.github.com>
2022-11-03 22:28:37 -0300

    Update Navbar.tsx

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Navbar.css';
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box style={{cursor: "pointer"}}>
+                        <Typography variant="h5" color="inherit">
+                            DiverCitta
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                               Sobre Nós
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Login
+                            </Typography>
+                        </Box>
+
+
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
