f0e55aeb0308b5fb61cde56297e8729db12c46e9
CristalyA <crisgavnml@gmail.com>
2022-11-22 15:20:54 -0300

    Ajuste footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
 import { Typography, Grid } from '@material-ui/core';
 import { Box } from '@mui/material';
 import { useSelector } from 'react-redux';
@@ -43,19 +43,19 @@ function Footer() {
 						</Box>
 						<Box display="flex" alignItems="center" justifyContent="center">
 							<a
-								href="https://www.facebook.com/generationbrasil"
+								href="projetointegradorgrupo4gb@gmail.com"
 								target="_blank"
 							>
-								<FacebookIcon style={{ fontSize: 60, color: 'white' }} />
+								<MailIcon style={{ fontSize: 60, color: 'white' }} />
 							</a>
 							<a
-								href="https://www.instagram.com/generationbrasil/"
+								href="https://www.instagram.com/divercitta_/"
 								target="_blank"
 							>
 								<InstagramIcon style={{ fontSize: 60, color: 'white' }} />
 							</a>
 							<a
-								href="https://www.linkedin.com/school/generationbrasil/"
+								href="https://www.linkedin.com/in/divercitta/"
 								target="_blank"
 							>
 								<LinkedInIcon style={{ fontSize: 60, color: 'white' }} />
